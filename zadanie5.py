524188233a966b12cf6bdfb9c14cf6bf25bf5205
djdanto <damianpiszczan@gmail.com>
2020-05-30 09:38:57 +0200

    message1

---
+++ zadanie5.py
@@ -0,0 +1,11 @@
+
+celsiusz = float(input('Wprowadz temeperature w stopniach celsiusza: '))  
+   
+fahrenheit = (celsiusz * 1.8) + 32  
+print('%0.1f  Celsiusz = %0.1f Fahrenheit'%(celsiusz,fahrenheit))
+
+  
+kw = float(input('Wprowadz ilość koni mechanicznych: '))  
+    
+kM = (kw * 1,36) 
+print(' kw = kM'%(kw,kM))
